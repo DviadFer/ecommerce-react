27bd300296b56a252186aa8127aa31df5f07b651
diego.viador <dviadorf@fpcoruna.afundacion.org>
2022-05-28 22:05:48 +0200

    Maquetada la barra de navegación
    Se han implementado los modulos de material UI para la vosualización de sus iconos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,88 @@
+import { Search, ShoppingCartOutlined } from '@material-ui/icons'
+import { Badge } from '@material-ui/core'
 import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-    height: 60px;
-    background-color: black;
+  height: 60px;
+`
+// Divisor de la barra de navegación
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  gap: 25px;
+`
+
+//Contenido de la barra de navegación
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`
+
+const Input = styled.input`
+  border: none;
+  &:focus {
+    outline: none;
+  }
+`
+const Logo = styled.h1`
+  font-weight: bold;
+`
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
 `
 
 const Navbar = () => {
   return (
     <Container>
-        Navbar
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input></Input>
+            <Search style={{color:"gray", fontSize: 16}}/>
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>ECOMMERCE.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+          <Badge badgeContent={8} color="primary">
+              <ShoppingCartOutlined />
+          </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
     </Container>
   )
 }
