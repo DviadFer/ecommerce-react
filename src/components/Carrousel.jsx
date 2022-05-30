828dd69c3b3d980ce6776e5e3470f3965be92cbc
Diego Viador Ferreira <diegoviadfer@gmail.com>
2022-05-30 13:55:27 +0200

    Creada la template de una slider individual. Imagen, texto y link.

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -5,7 +5,6 @@ const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
-    background-color: salmon;
     position: relative;
 `
 
@@ -22,10 +21,44 @@ const Arrow = styled.div`
     bottom: 0;
     // Se modifíca el CSS en función de las propiedades del componente React <Arrow>
     left: ${props => props.direction === "left" && "10px"}; 
-    right: ${props => props.direction === "right" && "10px"};;
+    right: ${props => props.direction === "right" && "10px"};
     margin: auto;
     cursor: pointer;
+    opacity: 0.5;
 `
+
+const Wrapper = styled.div`
+    height: 100%;
+`
+
+//Estructura de cada pestaña
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+`
+const ImageContainer = styled.div`
+    height: 100%;
+    flex: 1;
+`
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`
+
+//Contenido de cada pestaña
+const Image = styled.img`
+    height: 80%;
+
+`
+
+const Title = styled.h1``
+const Description = styled.p``
+const Button = styled.button``
+
+
 
 const Carrousel = () => {
   return (
@@ -33,6 +66,18 @@ const Carrousel = () => {
         <Arrow direction="left">
             <ArrowLeft/>
         </Arrow>
+        <Wrapper>
+            <Slide>
+                <ImageContainer>
+                    <Image src="https://i.ibb.co/DG69bQ4/2.png" />
+                </ImageContainer>
+                <InfoContainer>
+                    <Title>SUMMER SALE</Title>
+                    <Description>DON'T COMPROMISE ON STYLE! GET FLAT 20% OFF FOR NEW ARRIVALS.</Description>
+                    <Button>SHOW NOW</Button>
+                </InfoContainer>
+            </Slide>
+        </Wrapper>
         <Arrow direction="right">
             <ArrowRight/>
         </Arrow>
