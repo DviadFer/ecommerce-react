921ab1b18291efb8f2e54eb447e1a40fd990aa6a
diego.viador <dviadorf@fpcoruna.afundacion.org>
2022-05-31 18:04:07 +0200

    Se ha preparado el componente CategoryItem para servir los datos del array categories
    Recibe los valores a traves de props desde Categories.jsx.
    También se han aplicado estilos CSS.

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,13 +1,52 @@
 import styled from "styled-components"
 
 const Container = styled.div`
+    flex: 1;
+    margin: 3px;
+    height: 70vh;
+    position: relative;
+`
 
+const Image = styled.img`
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+`
+
+const Info = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+const Title = styled.h1`
+    color: white;
+    margin-bottom: 20px;
+`
+
+const Button = styled.button`
+    border: none;
+    padding: 10px;
+    background-color: white;
+    color: gray;
+    font-weight: 600;
+    cursor: pointer;
 `
 
 function CategoryItem({item}) {
   return (
     <Container>
-        Test
+        <Image src={require('../assets/'+item.img+'.jpeg')} />
+        <Info>
+            <Title>{item.title}</Title>
+            <Button>SHOP NOW</Button>
+        </Info>
     </Container>
   )
 }
