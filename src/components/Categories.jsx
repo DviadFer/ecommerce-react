830c01f4da5dc5a7c90342483e4426892edb42ed
diego.viador <dviadorf@fpcoruna.afundacion.org>
2022-05-31 17:40:45 +0200

    Creados las templates de los componentes react necesarios para un display de categorías.

---
+++ src/components/Categories.jsx
@@ -0,0 +1,16 @@
+import { styled } from "@material-ui/core"
+import { categories } from "../data"
+
+const Container = styled.div`
+
+`
+
+const Categories = () => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default Categories
---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,15 @@
+import { styled } from "@material-ui/core"
+
+const Container = styled.div`
+
+`
+
+function CategoryItem() {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default CategoryItem
