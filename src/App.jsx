22e47825ad2b41b2b0aa3008aa9f85e0d2c8887b
diego.viador <dviadorf@fpcoruna.afundacion.org>
2022-06-01 20:49:58 +0200

    Añadido la funcionalidad de links entre páginas a traves del módulo React-Router
    La lógica de encapsulamiento se refleja ahora de manera integrada en App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import PromoBanner from './components/PromoBanner';
 import Home from "./pages/Home";
+import Shop from "./pages/Shop"
+import Cart from "./pages/Cart"
 
 const App = () => {
-    return <Home />;
-  };
+  return (
+    <Router>
+      <div>
+        <PromoBanner />
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  )
+}
   
-  export default App;
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import Carrousel from '../components/Carrousel'
 import Categories from '../components/Categories'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
 import Newsletter from '../components/Newsletter'
 import PopularProducts from '../components/PoupularProducts'
-import PromoBanner from '../components/PromoBanner'
 
 const Home = () => {
   return (
     <div>
-      <PromoBanner />
-      <Navbar />
       <Carrousel />
       <Categories />
       <PopularProducts />
       <Newsletter />
-      <Footer />
     </div>
   )
 }
