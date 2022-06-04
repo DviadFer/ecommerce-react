43c551876d752f0b4f75d22a200f679de88f9462
diego.viador <dviadorf@fpcoruna.afundacion.org>
2022-06-04 16:56:11 +0200

    Cart y la pagina single product ya son responsive. RESPONSIVE WEB terminada.

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -2,6 +2,8 @@ import { Add, ArrowLeft, Remove } from "@material-ui/icons";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+import { mobile } from "../responsive";
+
 const Container = styled.div`
   width: 90%;
   max-width: 1200px;
@@ -11,6 +13,7 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 50px;
   display: flex;
+  ${mobile({ padding: "10px", flexDirection:"column" })}
 `
 
 const ImgContainer = styled.div`
@@ -26,6 +29,7 @@ const Image = styled.img`
 const InfoContainer = styled.div`
   flex: 1;
   padding: 0px 50px;
+  ${mobile({ padding: "10px" })}
 `
 
 const Title = styled.h1`
@@ -46,6 +50,7 @@ const FilterContainer = styled.div`
   margin: 30px 0px;
   display: flex;
   justify-content: space-between;
+  ${mobile({ width: "100%" })}
 `
 
 const Filter = styled.div`
@@ -75,7 +80,7 @@ const FilterSize = styled.select`
 const FilterSizeOption = styled.option``;
 
 const AddContainer = styled.div`
-  width: 50%;
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -119,18 +124,19 @@ const Divider = styled.hr`
 `
 
 const Back = styled(Link)`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: auto;
-    width: fit-content;
-    text-decoration: none;
-    font-size: 15px;
-    color: black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+  width: fit-content;
+  text-decoration: none;
+  font-size: 15px;
+  color: black;
 
-    &:hover {
-        font-weight: bold;
-    }
+  &:hover {
+      font-weight: bold;
+  }
+  ${mobile({ marginBottom: "25px" })}
 `
 
 const Product = () => {
