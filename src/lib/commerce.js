6aff3b14e56802b6467c61933135b8c6db3e6b7f
diego.viador <dviadorf@fpcoruna.afundacion.org>
2022-06-06 02:07:46 +0200

    FIX: Se ha añadido .env.
    Por defecto está en git ignore y no se había subido al repositorio

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,5 +1,5 @@
 import Commerce from '@chec/commerce.js'
 
 //REACT_APP_CHEC_PUBLIC_KEY así es como llame a la variable que encapsula la clave para conectarse a la API rest commerce.js
-//Estará almacenada en un fichero .env (eviromental variables) oculto que no detecta git
+//Estará almacenada en un fichero .env (eviromental variables) oculto
 export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
